f44af9235abdbf092e2b302703397b8159a17538
jburlama <jburlama@vbox.home>
2025-05-05 21:25:49 +0100

    add adminer

--- requirements/wordpress/wp-install.sh
+++ requirements/wordpress/wp-install.sh
@@ -5,4 +5,9 @@ tar -xzvf latest.tar.gz
 
 mv wordpress/* /var/www/html/.
 rm -rf wordpress latest.tar.gz
-cp wp-config.php /var/www/html/.
+mv wp-config.php /var/www/html/.
+
+wget http://www.adminer.org/latest.php -O adminer.php
+chown -R www-data:www-data adminer.php
+chmod 755 adminer.php
+mv adminer.php /var/www/html/.
