b8a73bcbd285f91f54129d15d87b4e81312ce283
jburlama <jburlama@vbox.home>
2025-05-06 22:12:09 +0100

    redis-cache

--- requirements/wordpress/wp-config.php
+++ requirements/wordpress/wp-config.php
@@ -38,6 +38,12 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+
+define( 'WP_REDIS_HOST', 'redis' );
+define( 'WP_REDIS_PORT', 6379 );     
+
+define('WP_CACHE', true);
+
 /**#@+
  * Authentication unique keys and salts.
  *
@@ -49,14 +55,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         'zh;|yVI;%>*XTBx@S|N|2pd++h^{<S9s6GrHAP(d0Z>G-bzmeGpT}%& OQn=mWRU');
+define('SECURE_AUTH_KEY',  'nk-(6~gr1t/g/F%~&7^^+o^N^qx70|tl:`PFUwMQJbq mE/t4-8K</{#BO$,=qNC');
+define('LOGGED_IN_KEY',    ') JPuau*GjuU6H5_t}f;`;H0=3:OLFBU-.ful~(.aItVGP1dR!s3N6wC`leLN:0q');
+define('NONCE_KEY',        '`H[9x4JK#>q$EVQ|e+G1G)Y{^fx/E[hZi+E&WX3Pr<R.3:R aZU7YFkY K>|9`g(');
+define('AUTH_SALT',        '4U}?{(6l-7`xv=[jO. e9sm]`VDt6RN!akWL0#X,TL<I&Wvgz-h$}-Sqd7+f:gWi');
+define('SECURE_AUTH_SALT', '*rIZ}$L8 .M$20[9pW,>* Ijs`#1zKNy<cG-1J>yrU|?r|pyYm2*yQL}y~;>&|^j');
+define('LOGGED_IN_SALT',   'GuS,wFfx_D20|a*Pg9r8leEoZ^x-0/6E:j%I4`S)n(Y2e2oe+;+DURj0syFy|d[u');
+define('NONCE_SALT',       'jbaU,@kEX{vn~gP08=xc3KR^aK/atvF0|QQ*$f8q7p&oBIOHdpy)u0qAAyu{3Q+w');
 
 /**#@-*/
 
--- requirements/wordpress/wp-install.sh
+++ requirements/wordpress/wp-install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp-cli
 
-mv wordpress/* /var/www/html/.
-rm -rf wordpress latest.tar.gz
-mv wp-config.php /var/www/html/.
+wp-cli core download --allow-root
+wp-cli plugin install redis-cache --activate --allow-root
+wp-cli plugin update --all --allow-root
 
 wget http://www.adminer.org/latest.php -O adminer.php
 chown -R www-data:www-data adminer.php
 chmod 755 adminer.php
-mv adminer.php /var/www/html/.
+
